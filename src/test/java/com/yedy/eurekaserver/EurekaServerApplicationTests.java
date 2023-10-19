d87e7b8653ae165bae1152f4ce653f5f007b549a
Yusuf Demircan <yusufdemircanysf@gmail.com>
2023-10-19 16:20:37 +0300

    Initial commit

---
+++ src/test/java/com/yedy/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yedy.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
